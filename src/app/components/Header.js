ebb7cbfa0b15487d83a84c819b7fc915f6847dbc
sravan <sravan.k.katta@student.fh-kiel.de>
2017-11-25 17:54:56 +0100

    Change Name from child to Parent using method

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import protoTypes from "prop-types";
 
 export class Header extends React.Component {
 	constructor(props) {
 		super();
 		this.state = {
-			age : props.initialAge
+			age : props.initialAge,
+			name: "Sravan"
 		}
 	}
 
@@ -14,14 +16,26 @@ export class Header extends React.Component {
 			age: this.state.age + 3
 		});
 	}
+
+	changeTitle() {
+		this.props.changeName(this.state.name)
+	}
 	render() {
 		return(
 			<div>
 				<p>Hello { this.props.name }</p>
 				<p>Your Age is { this.state.age }</p>
 				<button onClick={ () => this.addAge() }>Increase age</button>
+				<button onClick={this.props.greet}>Greet</button>
+				<button onClick={ this.changeTitle.bind(this) }>Change Name</button>
 			</div>
 			);
 	}
 }
 
+
+Header.ProtoTypes = {
+	name: protoTypes.string,
+	age: protoTypes.number,
+	greet: protoTypes.func
+}
--- src/app/index.js
+++ src/app/index.js
@@ -5,10 +5,28 @@ import {Header} from "./components/Header";
 import {Home} from "./components/Home";
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			name: "John"
+		}
+	}
+	onGreet() {
+		alert("Hello!");
+	}
+
+	changeName(newName) {
+		this.setState({
+			name: newName
+		});
+	}
+
+	
+
 	render() {
 		return(
 			<div className="container">
-				<Header name={"John"} initialAge={24} /> {/*Props*/}
+				<Header name={this.state.name} initialAge={24} greet={this.onGreet} changeName = {this.changeName.bind(this)}/> {/*Props*/}
 				<Home  />
 			</div>
 			);
